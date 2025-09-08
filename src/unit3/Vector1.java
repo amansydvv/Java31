b5ece05c00b02c4ad2a268cb4ff276129fb0e628
amansydvv <amansydv@gmail.com>
2025-09-08 14:48:34 +0530

    hi

---
+++ src/unit3/Vector1.java
@@ -0,0 +1,18 @@
+package unit3;
+import java.util.Vector;
+
+public class Vector1 {
+    public static void main(String[] args) {
+        Vector <String> vt = new Vector<>(5,5);
+        vt.add("First");
+        vt.add("Second");
+        vt.add("First");
+        vt.add("Second");
+        vt.add("First");
+        vt.add("Second");
+        vt.add(2,"forth");
+        System.out.println(vt.capacity());
+        System.out.println(vt.size());;
+    }
+
+}
